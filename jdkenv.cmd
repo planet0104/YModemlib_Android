6b0c9389e55b1fab18823e4247151e0076b29f5e
Jia Ye <planet2@qq.com>
2025-08-06 12:55:12 +0800

    构建aar命令

--- jdkenv.cmd
+++ jdkenv.cmd
@@ -1,2 +1,6 @@
 set JAVA_HOME=C:\Program Files\Android\Android Studio2021\jre
 set PATH=C:\Program Files\Android\Android Studio2021\jre\bin;%PATH%
+
+
+$env:JAVA_HOME = "C:\Program Files\Android\Android Studio2021\jre";
+$env:PATH = "C:\Program Files\Android\Android Studio2021\jre\bin;" + $env:PATH; java -version
